dd9a29a37b6569b39d260929a21a2d140aa09bd6
Frankline Isaboke <isabokefrankline@gmail.com>
2023-12-28 14:34:43 +0300

    the find method

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+
+function superbowlWin(record) {
+  let winningYear = record.find((obj) => obj.result === "W");
+
+  if (winningYear) {
+    return winningYear.year;
+  }
+}
